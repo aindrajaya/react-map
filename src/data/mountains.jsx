a380f512bd9610979d22f6a0ccbb94764afc25b7
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-20 05:53:00 +0700

    add mountain data

---
+++ src/data/mountains.jsx
@@ -0,0 +1,110 @@
+export const mountains = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Everest",
+        continent: "Asia",
+        elevation: 8848,
+        color: "#039c4b"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [86.925145, 27.988257],
+      },
+      id: "538fb51c-8396-4561-87c7-c7e30f0871ed",
+    },
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Denali",
+        continent: "North America",
+        elevation: 6194,
+        color: "#f16623"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-151.1926, 63.1148],
+      },
+      id: "0fe0217b-6674-4802-9f4b-d843be4b6a1b",
+    },
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Aconcagua",
+        continent: "South America",
+        elevation: 6961,
+        color: "#fedf17"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-70.010864, -32.653179],
+      },
+      id: "c2c65f1c-9ac6-4299-8f80-4dfbb018a400",
+    },
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Kilimanjaro",
+        continent: "Africa",
+        elevation: 5895,
+        color: "#f44546"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [37.353055555556, -3.0755555555556],
+      },
+      id: "40ad69a4-cd53-46a2-a0e0-1259d66b4ea6",
+    },
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Elbrus",
+        continent: "Europe",
+        elevation: 5642,
+        color: "#66d313"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [42.439167, 43.355],
+      },
+      id: "4134138a-4c37-45a9-b214-84ba8b769691",
+    },
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Vinson",
+        continent: "Antartica",
+        elevation: 4892,
+        color: "#ff0984"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-85.6172222, -78.5252778],
+      },
+      id: "b3e993a6-a943-4f88-a3fb-5ed5ab11d543",
+    },
+    {
+      type: "Feature",
+      properties: {
+        shape: "Marker",
+        name: "Mount Wilhelm",
+        continent: "Oceania",
+        elevation: 4509,
+        color: "#21409a"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [145.029543, -5.780065],
+      },
+      id: "9943070e-46ba-4b97-b0f6-72ce62ad6019",
+    },
+  ],
+};
