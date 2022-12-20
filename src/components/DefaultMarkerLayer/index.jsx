ed6a147c1f7bf99436f65f00b9e64847999235aa
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-21 05:09:00 +0700

    implementing ant design

--- src/components/DefaultMarkerLayer/index.jsx
+++ src/components/DefaultMarkerLayer/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 
+//Styling
+import { Card } from 'antd';
+
 const DefaultMarkerLayer = ({data, icon}) => {
   return data.features?.map((feature, idx) => {
     const {coordinates} = feature.geometry;
@@ -8,8 +11,11 @@ const DefaultMarkerLayer = ({data, icon}) => {
 
     return (
       <Marker key={idx} position={[coordinates[1], coordinates[0]]} icon={icon}>
+        {/* <PopupStatistics /> */}
         <Popup>
-          This is the town of {name} from Country {sov0name}. <br />You can customize it
+          <Card type='inner' title={`${name} from ${sov0name}`}>
+            Inner Card content
+          </Card>
         </Popup>
       </Marker>
     )
