9cbc66412914d0c1dabeea5137ed11e0d34d0db5
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-21 05:15:00 +0700

    split components

--- src/components/DefaultMarkerLayer/index.jsx
+++ src/components/DefaultMarkerLayer/index.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 
-//Styling
-import { Card } from 'antd';
+import CardPopup from '../CardPopup';
 
 const DefaultMarkerLayer = ({data, icon}) => {
   return data.features?.map((feature, idx) => {
     const {coordinates} = feature.geometry;
-    const {name, sov0name} = feature.properties
 
     return (
       <Marker key={idx} position={[coordinates[1], coordinates[0]]} icon={icon}>
         {/* <PopupStatistics /> */}
         <Popup>
-          <Card type='inner' title={`${name} from ${sov0name}`}>
-            Inner Card content
-          </Card>
+          <CardPopup properties={feature.properties}/>
         </Popup>
       </Marker>
     )
