a754f2eeb464c52821e694be216e8439d91b12de
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-22 05:12:00 +0700

    create geojson polygon component #1

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as DefaultMarkerLayer} from './DefaultMarkerLayer';
 export {default as TooltipMarkerLayer} from './TooltipMarkerLayer';
-export {default as RadiusFilterLayer} from './RadiusFilterLayer'
+export {default as RadiusFilterLayer} from './RadiusFilterLayer';
+export {default as ContinentsPolygonLayer} from './ContinentsPolygonLayer';
