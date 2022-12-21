74a7d39741ebff1cb7c9e8b97c1b6305fbaa58c0
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-12-22 05:21:00 +0700

    create function to get continents

--- src/components/ContinentsPolygonLayer/index.jsx
+++ src/components/ContinentsPolygonLayer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { GeoJSON } from 'react-leaflet';
 
-const ContinentsPolygonLayer = ({data}) => {
+const ContinentsPolygonLayer = ({data, setGeoFilter, getGeoFilter}) => {
+  const geoFilter = getGeoFilter;
+
   return (
-    <GeoJSON key='geo-json-layer' data={data}>
+    <GeoJSON 
+      key='geo-json-layer' 
+      data={data} 
+      eventHandlers={{
+        click: (e) => setGeoFilter(e.propagatedFrom.feature)
+      }}
+      >
 
     </GeoJSON>
   )
